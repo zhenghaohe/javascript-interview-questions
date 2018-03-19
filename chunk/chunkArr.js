11995a3a20ee7bce66dd0064b8c8e350724f62bb
zhenghao <iron4wine@outlook.com>
2018-03-18 22:23:57 -0700

    update

---
+++ chunk/chunkArr.js
@@ -0,0 +1,31 @@
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+function chunk(arr,size) {
+  const chunked  = [];
+  for (let element of arr) {
+    const last = chunked[chunked.length-1];
+    if (!last || last.length === size) {
+      chunked.push([element]);
+    }else{
+      last.push(element);
+    }
+  }
+  return chunked;
+}
+
+function chunk2(arr,size) {
+  const chunked = [];
+  let index = 0;
+  while(index<arr.length) {
+    chunked.push(arr.slice(index,index+size+1)); // directly stick it into chunked
+    index += size;
+  }
+  return chunked;
+}
