fe8ea83873e3aa4fe6c8ee80924e4d136d5a2f1b
zhenghao <iron4wine@outlook.com>
2018-03-19 19:55:42 -0700

    update

---
+++ queue/queue&weaveQueue.js
@@ -0,0 +1,64 @@
+// --- Description
+// Create a queue data structure.  The queue
+// should be a class with methods 'add' and 'remove'.
+// Adding to the queue should store an element until
+// it is removed
+// --- Examples
+//     const q = new Queue();
+//     q.add(1);
+//     q.remove(); // returns 1;
+
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  enqueue(record) {
+    this.data.unshift(record);
+  }
+
+  pop() {
+    return this.data.pop();
+  }
+
+  peak() {
+    return this.data[this.data.length-1];
+  }
+}
+
+// --- Directions
+// 1) Complete the task in weave/queue.js
+// 2) Implement the 'weave' function.  Weave
+// receives two queues as arguments and combines the
+// contents of each into a new, third queue.
+// The third queue should contain the *alterating* content
+// of the two queues.  The function should handle
+// queues of different lengths without inserting
+// 'undefined' into the new one.
+// *Do not* access the array inside of any queue, only
+// use the 'add', 'remove', and 'peek' functions.
+// --- Example
+//    const queueOne = new Queue();
+//    queueOne.add(1);
+//    queueOne.add(2);
+//    const queueTwo = new Queue();
+//    queueTwo.add('Hi');
+//    queueTwo.add('There');
+//    const q = weave(queueOne, queueTwo);
+//    q.remove() // 1
+//    q.remove() // 'Hi'
+//    q.remove() // 2
+//    q.remove() // 'There'
+
+function weave(queueA,queueB) {
+  const result = new Queue();
+  while (queueA.peak()||queueB.peak()) {
+    if (queueA.peak()) {
+      result.enqueue(queueA.pop());
+    }
+    if (queueB.peak()) {
+      result.enqueue(queueB.pop());
+    }
+  }
+  return result;
+}
