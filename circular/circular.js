865af4553e4b03a2ea14e72b5c72c5fce0881f52
zhenghao <iron4wine@outlook.com>
2018-03-21 10:03:02 -0700

    update

---
+++ circular/circular.js
@@ -0,0 +1,25 @@
+// --- Directions
+// Given a linked list, return true if the list
+// is circular, false if it is not.
+// --- Examples
+//   const l = new List();
+//   const a = new Node('a');
+//   const b = new Node('b');
+//   const c = new Node('c');
+//   l.head = a;
+//   a.next = b;
+//   b.next = c;
+//   c.next = b;
+//   circular(l) // true
+function circular(list) {
+  let slow = list.head;
+  let fast = list.head;
+  while(fast.next && fast.next.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+    if (slow === fast) {
+      return true;
+    }
+  }
+  return false;
+}
