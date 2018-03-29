b839fff8fd7e5ca1563cebd7b88614277eab9035
zhenghao <iron4wine@outlook.com>
2018-03-28 23:32:37 -0700

    update

--- reverseString/reversestr.js
+++ reverseString/reversestr.js
@@ -2,13 +2,18 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
 //the easiest way
 function reverse(str) {
   return str.split('').reverse().join('');
+}
+
+//use "reduce" helper
+function reverse2(str) {
+  return str.split('').reduce((newStr,char)=>char+newStr,'');
 }
 
 //iterative solution
@@ -20,7 +25,8 @@ function reverse3(str) {
   return newStr;
 }
 
-//use "reduce" helper
-function reverse2(str) {
-  return str.split('').reduce((newStr,char)=>char+newStr,'');
+//the recursive way
+function reverse4(str) {
+  if (str === '') return '';
+  return reverse4(str.substr(1))+str[0] // or str.slice(1)
 }
