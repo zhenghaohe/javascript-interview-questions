0eb9444efcd7ec83d658236a4ca84ea81d787c57
zhenghao <iron4wine@outlook.com>
2018-03-29 22:02:25 -0700

    update

--- removeDup/removeDup.js
+++ removeDup/removeDup.js
@@ -1,4 +1,14 @@
 //remove duplicates in string
+
+//use filter helper
 function removeDup(str) {
   return str.split(' ').filter((val,index,self) => self.indexOf(val) === index).join(' ');
+}
+
+//use reduce and find helper
+function removeDup2(string) {
+  return string.toLowerCase().split(' ').reduce((previous,word)=>{
+    if (!previous.find(prev => prev===word)) previous.push(word);
+    return previous;
+  },[]).join(' ');
 }
