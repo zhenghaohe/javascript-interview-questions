4ed1e5fa659d5146d40d88847802469542786737
zhenghao <iron4wine@outlook.com>
2018-03-31 14:29:39 -0700

    update

--- bst/bst2.js
+++ bst/bst2.js
@@ -32,7 +32,7 @@ class BST {
 
   findMin() {
     let current = this.root;
-    while (current.left !== null) {
+    while (current.left) {
       current = current.left;
     }
     return current.data;
@@ -40,7 +40,7 @@ class BST {
 
   findMax() {
     let current = this.root;
-    while (current.right !== null) {
+    while (current.right) {
       current = current.right;
     }
     return current.data;
@@ -54,9 +54,7 @@ class BST {
       } else {
         current = current.right;
       }
-      if (current === null) {
-        return null;
-      }
+      if (!current) return null;
     }
     return current;
   }
@@ -77,21 +75,19 @@ class BST {
   }
 
   remove(data) {
-    const removeNode = function(node, data) {
-      if (node == null) {
-        return null;
-      }
-      if (data == node.data) {
+    (function removeNode(node, data) {
+      if (!node) return null;
+      if (data === node.data) {
         // node has no children
-        if (node.left == null && node.right == null) {
+        if (!node.left && !node.right) {
           return null;
         }
         // node has no left child
-        if (node.left == null) {
+        else if (!node.left) {
           return node.right;
         }
         // node has no right child
-        if (node.right == null) {
+        else if (!node.right) {
           return node.left;
         }
         // node has two children
@@ -109,8 +105,7 @@ class BST {
         node.right = removeNode(node.right, data);
         return node;
       }
-    }
-    this.root = removeNode(this.root, data);
+    })(this.root, data);
   }
 
   isBalanced() {
@@ -119,83 +114,76 @@ class BST {
 
   findMinHeight(node = this.root) {
     if (!node) return -1;
-      const left = this.findMinHeight(node.left);
-      const right = this.findMinHeight(node.right);
-      return ( left<right? left+1: right+1);
+    return Math.min(this.findMinHeight(node.left),this.findMinHeight(node.right))+1;
   }
 
   findMaxHeight(node = this.root) {
       if (!node) return -1;
-      const left = this.findMaxHeight(node.left);
-      const right = this.findMaxHeight(node.right);
-      return( right>left ? right+1 : left+1)
+      return Math.max(this.findMaxHeight(node.left),this.findMaxHeight(node.right))+1;
   }
 
   inOrder() {
-    if (this.root == null) {
-      return null;
-    } else {
-      var result = new Array();
-      function traverseInOrder(node) {
-        node.left && traverseInOrder(node.left);
-        result.push(node.data);
-        node.right && traverseInOrder(node.right);
-      }
-      traverseInOrder(this.root);
-      return result;
-    };
+    if (!this.root) return null;
+    const result = [];
+    (function traverseInOrder(node) {
+      node.left && traverseInOrder(node.left);
+      result.push(node.data);
+      node.right && traverseInOrder(node.right);
+    })(this.root)
+    return result;
   }
 
   preOrder() {
-    if (this.root == null) {
-      return null;
-    } else {
-      var result = new Array();
-      function traversePreOrder(node) {
-        result.push(node.data);
-        node.left && traversePreOrder(node.left);
-        node.right && traversePreOrder(node.right);
-      };
-      traversePreOrder(this.root);
-      return result;
-    };
+    if (!this.root) return null;
+    const result = [];
+    (function traversePreOrder(node) {
+      result.push(node.data);
+      node.left && traversePreOrder(node.left);
+      node.right && traversePreOrder(node.right);
+    })(this.root)
+    return result;
   }
 
   postOrder() {
-    if (this.root == null) {
-      return null;
-    } else {
-      var result = new Array();
-      function traversePostOrder(node) {
-        node.left && traversePostOrder(node.left);
-        node.right && traversePostOrder(node.right);
-        result.push(node.data);
-      };
-      traversePostOrder(this.root);
-      return result;
-    }
+    if (!this.root) return null;
+    const result = [];
+    (function traversePostOrder(node) {
+      node.left && traversePostOrder(node.left);
+      node.right && traversePostOrder(node.right);
+      result.push(node.data);
+    })(this.root);
+    return result;
   }
 
-  levelOrder() {
-      let result = [];
-      let Q = [];
-      if (this.root != null) {
-          Q.push(this.root);
-          while(Q.length > 0) {
-              let node = Q.shift();
-              result.push(node.data);
-              if (node.left != null) {
-                  Q.push(node.left);
-              };
-              if (node.right != null) {
-                  Q.push(node.right);
-              };
-          };
-          return result;
-      } else {
-          return null;
-      };
-  };
+  traverseBF() {
+    const result = [];
+    if (this.root) {
+      const queue = [this.root];
+      while(queue.length) {
+        const node = queue.shift();
+        if(node.left) queue.push(node.left);
+        if(node.right) queue.push(node.right);
+        result.push(node.data);
+      }
+      return result;
+    }
+    return null;
+  }
+
+  traverseDF() {
+    const result = [];
+    if (this.root) {
+      const stack = [this.root];
+      while(stack.length) {
+        const node = stack.shift();
+        if(node.right) stack.unshift(node.right);
+        if(node.left) stack.unshift(node.left);
+        result.push(node.data);
+      }
+      return result;
+    }
+    return null;
+  }
 }
 
 
@@ -213,7 +201,11 @@ bst.add(5);
 bst.add(9);
 bst.add(27);
 
-
+// bst.remove(5);
+bst.remove(7);
+bst.remove(15);
+console.log(bst.findMax());
+console.log(bst.findMin());
 console.log(bst.findMinHeight());
 console.log(bst.findMaxHeight());
 console.log(bst.isBalanced());
@@ -225,5 +217,5 @@ console.log(bst.isBalanced());
 console.log('inOrder: ' + bst.inOrder());
 console.log('preOrder: ' + bst.preOrder());
 console.log('postOrder: ' + bst.postOrder());
-
-console.log('levelOrder: ' + bst.levelOrder());
+console.log('traverseBF: ' + bst.traverseBF());
+console.log('traverseDF: ' + bst.traverseDF());
