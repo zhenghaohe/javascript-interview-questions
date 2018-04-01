4e687a5b8d6f007f1842c494d8dd5bf8b2916baf
zhenghao <iron4wine@outlook.com>
2018-03-31 22:33:43 -0700

    update

---
+++ reversewords/reverseWords.js
@@ -0,0 +1,20 @@
+//‘Coding JavaScript -> 'gnidoC tpircSavaJ'
+function reverseWords(string) {
+  var wordsArr = string.split(' ');
+  var reversedWordsArr = [];
+  wordsArr.forEach(word=>{
+    reversedWordsArr.push(word.split('').reduce((newWord,char)=>char+newWord,''));
+  })
+  return reversedWordsArr.join(' ');
+}
+
+//‘Coding JavaScript -> 'JavaScript Coding'
+function reverseWords2(string) {
+  return string.split(' ').reduce((newStr,word)=>{
+    newStr.unshift(word);
+    return newStr;
+  },[]).join(' ');
+}
+
+console.log(reverseWords('Coding JavaScript'));
+console.log(reverseWords2('Coding JavaScript'));
