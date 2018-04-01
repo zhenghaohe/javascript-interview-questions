8a1a254134c8bd6a4488fa54f89a26a7c63fdc88
zhenghao <iron4wine@outlook.com>
2018-03-31 21:29:17 -0700

    update

---
+++ reverseint/reverseint.js
@@ -0,0 +1,28 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+function reverseInt(n) {
+  const reversed = n.toString().split('').reverse().join('');
+  return parseInt(reversed)*Math.sign(n);
+}
+
+function reverseInt2(n) {
+  let result = 0;
+  while(n) {
+    result = result*10 + n%10;
+    n = parseInt(n/10);
+  }
+  if(result> Math.pow(2,31) || -result>Math.pow(2,31)) result = 0;
+  return result;
+}
+
+
+console.log( reverseInt(-90));
+
+console.log(parseInt(9/10));
