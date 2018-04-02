d59fc6194d8f0adf4e6490a66850dc564d115a46
zhenghao <iron4wine@outlook.com>
2018-04-01 21:23:28 -0700

    update

---
+++ stringmatch/stringMatch.js
@@ -0,0 +1,12 @@
+//Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+var strStr = function(haystack, needle) {
+    if(!needle) return 0;
+    if(!haystack || needle.length > haystack.length) return -1;
+    for(let i = 0 ; i < haystack.length - needle.length + 1; i++){
+       var str = haystack.substr(i,needle.length);
+       if(str === needle){
+           return i;
+       }
+    }
+    return -1;
+};
