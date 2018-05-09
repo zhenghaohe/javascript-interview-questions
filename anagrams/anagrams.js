e183f15e0af56b66c8d5d1cb59fe4953fd5d1e46
zhenghao <iron4wine@outlook.com>
2018-05-09 12:20:12 -0700

    update

--- anagrams/anagrams.js
+++ anagrams/anagrams.js
@@ -8,27 +8,32 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+//solution 1 - use a hash table
 function anagrams(stringA,stringB) {
   const aCharMap = buildCharMap(stringA);
   const bCharMap = buildCharMap(stringB);
-  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-    return false;
-  }
+
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) return false;
+
   for (let char in aCharMap) {
     if (aCharMap[char] !== bCharMap[char]) return false;
-    }
+  }
+
   return true;
   }
 
 
 function buildCharMap(str) {
   const charMap = {};
+
   for (let char of str.replace(/[^\w]/g,"").toLowerCase()) { // or str.replace(/\W/g,'').toLowerCase()
     charMap[char]?charMap[char]++:charMap[char]=1;
   }
+
   return charMap;
 }
 
+//solution 2 - sort the strings
 function anagrams2(stringA,stringB) {
   return cleanString(stringA) === cleanString(stringB);
 }
