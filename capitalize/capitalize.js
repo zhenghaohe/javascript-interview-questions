590d62ed9dad52e4f83e973b2522b1e12715dbf3
zhenghao <iron4wine@outlook.com>
2018-05-11 10:34:33 -0700

    update

--- capitalize/capitalize.js
+++ capitalize/capitalize.js
@@ -1,4 +1,3 @@
-// --- Directions
 // Write a function that accepts a string.  The function should
 // capitalize the first letter of each word in the string then
 // return the capitalized string.
@@ -9,21 +8,21 @@
 
 function capitalize(str) {
   const words = [];
+
   for (let word of str.split(' ')) {
     words.push(word[0].toUpperCase()+word.slice(1));
   }
+
   return words.join(' ');
 }
 
 function capitalize2(str) {
   let words = str[0].toUpperCase();
+
   for (let i = 1; i < str.length; i++) {
-    if (str[i-1]===' ') { //if str[i] is the first char of a word
-      words += str[i].toUpperCase();
-    }else{
-      words += str[i];
-    }
+    words += str[i-1] === ' ' ? str[i].toUpperCase():str[i];
   }
+
   return words;
 }
 
