1211516c3ca155d23a797f250f5e9086edb6273d
zhenghao <iron4wine@outlook.com>
2018-06-03 11:08:22 -0700

    update

---
+++ Set/Set.js
@@ -0,0 +1,47 @@
+// implement a mathematic Set
+class Set {
+  constructor() {
+    this._set = {};
+  }
+
+  add(obj){ // O(1)
+    let str = this.hashFunc(obj);
+    if (!this.checkDup(str)) {
+      this._set[str] = obj;
+    }
+    return this;
+  }
+
+  hashFunc(obj) {
+    return `${obj.name}&${obj.id}`;
+  }
+
+  delete(obj) { // O(1)
+    let str = this.hashFunc(obj);
+    if (this.checkDup(str)) {
+      delete this._set[str];
+    }
+  }
+
+  has(obj) { // O(1)
+    return !!this._set[this.hashFunc(obj)];
+  }
+
+  checkDup(str) { // O(1)
+    return this._set[str];
+  }
+}
+
+
+const set = new Set();
+set.add({name:"wangming",id:9874});
+console.log(set);
+set.delete({name:"wangming",id:9874});
+console.log(set);
+set.add({name:"zhenghao",id:1234}).add({name:"Ambar",id:2345});
+console.log(set);
+console.log(set.has({name:"zhenghao",id:1234}));
+set.delete({name:"zhenghao",id:1234});
+console.log(set);
+console.log(set.has({name:"zhenghao",id:1234}));
+console.log(set.has({name:"Ambar",id:2345}));
