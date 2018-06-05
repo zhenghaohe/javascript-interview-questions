38048b25590483875c3724fc8d5c454f397827ce
zhenghao <iron4wine@outlook.com>
2018-06-04 20:57:38 -0700

    update

--- removeWhitespace/removeWhitespaces.js
+++ removeWhitespace/removeWhitespaces.js
@@ -1,11 +1,27 @@
-const removeWhiteSpaces = str => str.split(' ').join('')
-const removeWhiteSpaces2 = str => {
-  let arr = [];
-  for (let i = 0; i < str.length; i++) {
-    if (str[i]!==' ') arr.push(str[i]);
-  }
-  return arr.join('');
-}
-const removeWhiteSpaces3 = str => str.replace(/\s/g,"")
+// "    And   nothing hurts anymore, /t  I feel    kinda free.  /t We're    still    the   kids  /t we used to be,   yeah,   yeah"
+//And nothing hurts anymore, I feel kinda free. We're still the kids we used to be, yeah, yeah
 
-console.log(removeWhiteSpaces3('    zhen     h '));
+const removeWhiteSpaces = str => {
+  let newStr = '';
+
+  str = str.trim();
+  newStr = str[0];
+  for (let i = 1; i < str.length; i++) {
+    if (str[i]===' ' || str[i]==="/t") {
+      if (str[i-1] !== ' ' && str[i-1]!=='/t') {
+        newStr += str[i];
+      }
+    }
+    else {
+      newStr += str[i];
+    }
+  }
+
+  return newStr;
+}
+
+
+
+const str = "    And   nothing hurts anymore,   I feel    kinda free.   We're    still    the   kids   we used to be,   yeah,   yeah";
+const str2 = "And nothing hurts anymore, I feel kinda free. We're still the kids we used to be, yeah, yeah";
+console.log(removeWhiteSpaces(str)===str2);
