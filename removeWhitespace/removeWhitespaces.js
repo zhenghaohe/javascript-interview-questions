c5b96ee9fa3215468729ffd0f996741f166320bb
zhenghao <iron4wine@outlook.com>
2018-06-04 22:22:36 -0700

    update

--- removeWhitespace/removeWhitespaces.js
+++ removeWhitespace/removeWhitespaces.js
@@ -1,4 +1,4 @@
-// "    And   nothing hurts anymore, /t  I feel    kinda free.  /t We're    still    the   kids  /t we used to be,   yeah,   yeah"
+// "    And   nothing hurts anymore, /t  I feel    kinda free.  /t/t We're    still    the   kids  /t we used to be,   yeah,   yeah"
 //And nothing hurts anymore, I feel kinda free. We're still the kids we used to be, yeah, yeah
 
 const removeWhiteSpaces = str => {
@@ -7,9 +7,9 @@ const removeWhiteSpaces = str => {
   str = str.trim();
   newStr = str[0];
   for (let i = 1; i < str.length; i++) {
-    if (str[i]===' ' || str[i]==="/t") {
-      if (str[i-1] !== ' ' && str[i-1]!=='/t') {
-        newStr += str[i];
+    if (str[i]===' ' || str[i]=== "\t") {
+      if (str[i-1] !== ' ' && str[i-1] !== '\t' ) {
+        newStr += ' ';
       }
     }
     else {
@@ -24,11 +24,11 @@ const removeWhiteSpaces2 = str => {
   let newStr = '';
 
   for (var i = 0; i < str.length; i++) {
-    while (str[i] === ' ' || str[i] === '/t') {
+    while (str[i] === ' ' || str[i] === '\t') {
       i++;
     }
     newStr += ' ';
-    while (str[i] !== ' ' && str[i] !== '/t' && str[i] !== undefined) {
+    while (str[i] !== ' ' && str[i] !== '\t' && str[i] !== undefined) {
       newStr += str[i];
       i++;
     }
@@ -37,8 +37,29 @@ const removeWhiteSpaces2 = str => {
   return newStr.trim();
 }
 
+const removeWhiteSpaces3 = str => {
+  let newStr = '';
+  let count = 0;
+
+  str = str.trim();
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === ' ' || str[i]=== "\t") {
+      count = 1;
+    }
+    else {
+      if (count > 0) newStr += " ";
+      newStr += str[i];
+      count = 0;
+    }
+  }
+
+  return newStr;
+}
 
 
-const str = "    And   nothing hurts anymore,   I feel    kinda free.   We're    still    the   kids   we used to be,   yeah,   yeah";
+
+const str = "    And   nothing hurts anymore, \t  I feel    kinda free.  \t We're    still    the   kids  \t we used to be,   yeah,   yeah";
 const str2 = "And nothing hurts anymore, I feel kinda free. We're still the kids we used to be, yeah, yeah";
-console.log(removeWhiteSpaces2(str));
+const str3= "a  \t\tb"
+console.log(removeWhiteSpaces3(str));
