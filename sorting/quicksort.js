4a1601c7d4f18e501aa53b3258d2f803c5736c49
zha46 <zha46@sfu.ca>
2019-06-06 11:58:44 -0700

    update

--- sorting/quicksort.js
+++ sorting/quicksort.js
@@ -1,3 +1,7 @@
+// Time complexity :Best case - O(nlogn). Worse case - O(n^2)
+// In-place: space complexity - O(logn) for maintaining the call stack
+// Worse case happens when we always choose the rightmost element with an sorted array
+
 const quickSort = (arr,left = 0,right = arr.length-1) => {
   if (left >= right) return; //base case
   const index = partition(arr, left, right);
@@ -6,19 +10,16 @@ const quickSort = (arr,left = 0,right = arr.length-1) => {
 };
 
 const partition = (arr,left,right) => {
-  const pivot = arr[left];
-  let i = left+1; //split between elements bigger than pivot and less than pivot
-  for (let j = left; j <= right; j++) { //split between partioned part and unpartitioned partioned
-    if (pivot>arr[j]) {
-      let temp = arr[j];
-      arr[j] = arr[i];
-      arr[i++] = temp;
-    }
-  }
-  let temp = arr[--i];
-  arr[left] = temp;
-  arr[i] = pivot;
-  return i;
+ const pivot = arr[right];
+ let index = left;
+ for (let i = left; i < right ; i++) {
+   if (arr[i] < pivot) {
+     [arr[i], arr[index]] = [arr[index], arr[i]];
+     index++;
+   }
+ }
+ [arr[index], arr[right]] = [arr[right], arr[index]];
+ return index;
 }
 
 const arr1 = [3,4,5,7,3,1,3,4,6,8,3,1,1,4,7,9,3];
@@ -36,3 +37,18 @@ console.log(arr4);
 const arr5 = [5,4,3];
 quickSort(arr5);
 console.log(arr5);
+
+
+// Not in-place quicksort
+// space-complexity - O(n)
+const quickSort2 = arr => {
+  if (arr.length <= 1) return arr;
+  const pivot = arr[arr.length - 1];
+  const left = [];
+  const right = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    arr[i] < pivot ? left.push(arr[i]) : right.push(arr[i])
+  }
+
+  return [...quickSort2(left), pivot, ...quickSort2(right)];
+}
